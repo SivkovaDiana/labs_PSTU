01a3242536df1152e7e32ff6574a715c2f8b40f0
SivkovaDiana <92534589+SivkovaDiana@users.noreply.github.com>
2021-11-01 13:14:24 +0500

    Add files via upload

---
+++ Задачи по вводу, выводу/1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>                        
+using namespace std;
+int main()
+{
+
+    setlocale(LC_ALL, "Russian");
+    int a, b, res;
+    cout << "Введите числа " << endl;
+    cin >> a >> b;
+    res = a * b;
+    cout << "Произведение равно " << res << endl;
+    return 0;
+}
---
+++ Задачи по вводу, выводу/2.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>                        
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    char a, b, c, d, e;
+    cout << "Введите символы " << endl;
+    cin >> a >> b >> c >> d >> e;
+    cout << "Обратная последовательность " << e << d << c << b << a;
+    return 0;
+}
